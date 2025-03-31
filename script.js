6f0f1359c70965236eb057d91213fd6cb6bc0c28
kyungwon2won2 <ykw1230@naver.com>
2025-03-31 18:21:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+function phone(){
+  Swal.fire({
+    html : '<i class="bi bi-telephone-fill"></i> 010-4186-1829<br> <i class="bi bi-envelope-fill"></i> ykw1230@naver.com',
+    icon : "info",
+    confirmButtonText : "닫기"
+  });
+}
